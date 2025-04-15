5360eb226393ebd76d6fa6435e1259bf3e5762ef
Oishi Sakana <kaunghtetsan275@gmail.com>
2025-04-16 05:04:39 +0700

    changed CORS ORIGIN

--- afdd_backend/settings.py
+++ afdd_backend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    '0.0.0.0',
+    'https://hotel-iot-afdd-frontend.vercel.app',
     "http://localhost:3001",
     "http://localhost:3000",
     "http://localhost:5173", 
